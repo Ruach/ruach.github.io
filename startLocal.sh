7cdcc4b0ebd309dc5c1dd386ef7d0e80d7ef159b
jaehyuk <jhyuk.lee@outlook.com>
2023-11-17 17:34:27 -0500

    x

--- startLocal.sh
+++ startLocal.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+rm -rf ./_site
 bundle exec jekyll serve
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700258333';
+const cacheName = 'chirpy-1700260305';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {

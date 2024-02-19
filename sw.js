e32879232655a4b746a6bff120ec5e78a454a27f
jaehyuk <jlee3757@AdministorsMBP6.attlocal.net>
2024-02-19 01:56:38 -0500

    Deployed on 2024-02-19 01:56:32

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708322658';
+const cacheName = 'chirpy-1708325797';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {

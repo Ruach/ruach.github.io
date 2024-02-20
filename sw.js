dfd4911580506cb9b0ee54b700566f108f92881f
jaehyuk <jlee3757@AdministorsMBP6.attlocal.net>
2024-02-20 14:40:41 -0500

    Deployed on 2024-02-20 14:40:33

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708456326';
+const cacheName = 'chirpy-1708458040';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {

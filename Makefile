325b74a555efb852c576582d0d1bb1eba36b657f
jaehyuk <jlee3757@AdministorsMBP6.attlocal.net>
2024-02-19 19:13:38 -0500

    x

--- Makefile
+++ Makefile
@@ -3,12 +3,12 @@ CHIRPY_DIR := $(ROOT_DIR)/chirpy
 GH_PAGES_DIR := $(ROOT_DIR)/gh-pages
 DEPLOY_DATE := $(shell date +'%Y-%m-%d %H:%M:%S')
 
-.PHONY: build serve posts deploy clean_up 
+.PHONY: build serve posts assets deploy clean_up 
 
-build: chirpy gh-pages posts
+build: chirpy gh-pages posts assets
 	@cd $(CHIRPY_DIR) && bundle exec jekyll build
 
-local: chirpy gh-pages posts
+local: chirpy gh-pages posts assets
 	@cd $(CHIRPY_DIR) && bundle exec jekyll serve 
 
 chirpy: 
@@ -26,6 +26,12 @@ posts:
 	@if [ ! -d "$(CHIRPY_DIR)/_posts" ]; then \
 		ln -s $(ROOT_DIR)/_posts $(CHIRPY_DIR)/; \
 	fi
+
+assets:
+	@if [ ! -d "$(CHIRPY_DIR)/assets" ]; then \
+		ln -s $(ROOT_DIR)/assets $(CHIRPY_DIR)/; \
+	fi
+
 
 
 deploy: build
---
+++ assets/js/img/slides.js
@@ -0,0 +1,26 @@
+//From https://www.w3schools.com/howto/howto_js_slideshow.asp
+
+let slideIndex = 0;
+showSlides();
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000);
+}

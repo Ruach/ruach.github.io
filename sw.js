fd8fdfe3168958483b3b6786591da2e545d4bee4
Jaehyuk Lee <jaehyukl@nvidia.com>
2026-07-04 21:43:03 -0400

    Deployed on 2026-07-04 21:42:55

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1783215409';
+const cacheName = 'chirpy-1783215782';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
